baef0a7931c22da3ab4708f2faa059b23345dc43
Gabriella-alt <gabyrochar12@gmail.com>
2025-08-20 21:34:03 -0300

    meu primeiro projeto

---
+++ pratícas/01-primeiro-projeto/index.js
@@ -0,0 +1,19 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Gabriella"
+let sobrenome = "Rocha"
+
+console.log(nome + " " + sobrenome)
+
+let idade = "20"
+
+console.log("A idade do" + nome + " é " + idade)
+
+let exercicios = " 0,5"
+
+let trabalho = " 1,5 "
+
+let prova =  " 4,5 "
+
+let notacalculada = exercicios + trabalho + prova 
+console.log("NOTA A1:" + notacalculada)
